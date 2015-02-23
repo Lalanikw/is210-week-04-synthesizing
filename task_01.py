914d6d1ec86f0d14257f4aa7cc6f88dc50c27648
Lalani Werake <lalanikw@gmail.com>
2015-02-23 06:46:53 +0000

    task_01.py

--- task_01.py
+++ task_01.py
@@ -1,6 +1,7 @@
 #!/user/bin/env python
 # -*- coding: utf-8 _*_
 """if study."""
+
 print "Which base color, Seattle Gray or Manatee?"
 inp = raw_input()
 if( inp == "Seattle Gray"):
@@ -13,23 +14,15 @@ if( inp == "Seattle Gray"):
         inp = raw_input()
         if( inp == "Basically White"):
             HIGHLIGHT = inp        
-        elif(inp =="WHITE):
+        elif(inp =="WHITE"):
             HIGHLIGHT = inp
         else:
              print "Error color is not recognized"
-             return
-    elif(inp ==" Cumulus Nimbus):
-         ACCENT = inp
-    else:
-         print "Error color is not recognized"
-         return
-elif( inp == "Manatee"):
-    BASE = inp
-else:
-    print "Error color is not recognized"
-elif( inp == "Manatee"):
-    BASE = inp
-else:
-    print "Error color is not recognized"
     return
+elif(inp =="Cumulus Nimbus"):
+         ACCENT = inp
+else:
+         print "Error color is not recognized"
+    return
+
 print "Your selected colors are, %s, %s, and %s".format(BASE,ACCENT,HIGHLIGHT)
