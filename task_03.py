0c962fb6ba0bce0a5a3856dec11da8844dcfd999
Lalani Werake <lalanikw@gmail.com>
2015-02-23 06:35:47 +0000

    task_03.py

---
+++ task_03.py
@@ -0,0 +1,20 @@
+#!/user/bin/env python
+# _*_ coding: utf-8 _*_
+"""if study."""
+print "What is your name?"
+name = raw_input()
+print "What is your principle?"
+p = raw_input()
+print "For how many years is the loan?"
+years = raw_input()
+print "Are you prequilified?"
+pr = raw_intput()
+if(pq=="Yes" or pq=="Y"):
+ pq="Y"
+elif (pq=="No" or pq=="n"):
+    pq="n"
+else:
+    print "error accpetable answers are Acceptable answers for this are Yes, y, No, and n "
+    return
+else:
+    print "incorrect input"
